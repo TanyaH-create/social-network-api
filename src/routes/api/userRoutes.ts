fbc30b18969bd0a4cc539592383c2476b9747184
Tanya <dougtanyah@gmail.com>
2025-02-26 13:06:51 -0500

    feature/ added code to populate thought and friends fields

--- src/routes/api/userRoutes.ts
+++ src/routes/api/userRoutes.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express';
 const router = Router();
-import { getUsers, getSingleUser, createUser } from '../../controllers/userController.js';
+import { getUsers, getSingleUser, createUser, addFriend, removeFriend } from '../../controllers/userController.js';
 
 // /api/users
 router.route('/').get(getUsers).post(createUser);
 
 // /api/users/:userId
 router.route('/:userId').get(getSingleUser);
+
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId')
+  .post(addFriend) // Add a friend
+  .delete(removeFriend); // Remove a friend
 
 export default router;
